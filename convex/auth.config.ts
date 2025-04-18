81440781f561d261ac8b5ddfcb448092c299706d
shreyes-7 <shreyesjaiswal7@gmail.com>
2025-04-18 23:33:14 +0530

    Code pushed

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://comic-gecko-2.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/components/providers/ConvexClientProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function ConvexClientProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+}
+
+export default ConvexClientProvider;
